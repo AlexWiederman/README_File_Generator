19ed3930df8f900d270f41a60f8e9147ec722f75
Alex W <amwiederman15@gmail.com>
2023-01-14 08:09:53 -0500

    first commit adding files

---
+++ Develop/index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = ['What is the projects title?', 'What is the description?', 'What is the installation instructions?',
+'What is the usage?','List your collaborators.', 'Are there any Tests?','What is your Github username?','What is your email?'];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+}
+
+// Function call to initialize app
+init();
