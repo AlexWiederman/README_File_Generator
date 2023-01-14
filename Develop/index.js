d2cd0ae7515bdfb709fb29d8a93c2e0b03d1c534
Alex W <amwiederman15@gmail.com>
2023-01-14 09:14:45 -0500

    added inquirer questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,9 +2,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
+
 // TODO: Create an array of questions for user input
 const questions = ['What is the projects title?', 'What is the description?', 'What is the installation instructions?',
-'What is the usage?','List your collaborators.', 'Are there any Tests?','What is your Github username?','What is your email?'];
+    'What is the usage?', 'List your collaborators.', 'Are there any Tests?', 'Choose your License:', 'What is your Github username?', 'What is your email?'];
+const names = ['title', 'description', 'install', 'usage', 'collab', 'tests', 'license', 'github', 'email']
+const licenseList = ['Apache','GNU General Public v3.0', 'MIT']
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -13,7 +17,55 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: names[0],
+                message: questions[0],
+            },
+            {
+                type: 'input',
+                name: names[1],
+                message: questions[1],
+            },
+            {
+                type: 'input',
+                name: names[2],
+                message: questions[2],
+            },
+            {
+                type: 'input',
+                name: names[3],
+                message: questions[3],
+            },
+            {
+                type: 'input',
+                name: names[4],
+                message: questions[4],
+            },
+            {
+                type: 'input',
+                name: names[5],
+                message: questions[5],
+            },
+            {
+                type: 'list',
+                name: names[6],
+                message: questions[6],
+                choices: licenseList
+            },
+            {
+                type: 'input',
+                name: names[7],
+                message: questions[7],
+            },
+            {
+                type: 'input',
+                name: names[8],
+                message: questions[8],
+            },
+        ])
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,10 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+const { default: inquirer } = require("inquirer");
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
 
@@ -44,27 +50,28 @@ ${data.credits}
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+${data.license}
 
 ## Badges
 
 ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
 
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+ [shields.io](https://shields.io/). 
 
 ## Features
 
-If your project has a lot of features, list them here.
+${data.features}
 
 ## How to Contribute
 
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+Contributor Covenant Code of Conduct
 
 ## Tests
 
-
-
+${data.test}
 `;
+
+
 }
 
 module.exports = generateMarkdown;
